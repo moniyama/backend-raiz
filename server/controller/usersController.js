f4db0c128552bd409253afda65942b40d85074a2
Monica <monicasayama@gmail.com>
2021-01-22 20:03:10 -0300

    add users controller com jwt e bcryptjs

--- server/controller/usersController.js
+++ server/controller/usersController.js
@@ -1,14 +1,11 @@
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const models = require('../db/models')
+const jwt = require('jsonwebtoken')
+const error = require('../../utils.js')
 
 const rebuildObj = (arr) => {
   return arr.map(obj => {
-    const role = obj.role
-    obj.role = {
-      admin: obj.admin,
-      task: role
-    }
-    delete obj.dataValues.admin
+    delete obj.dataValues.password
     return obj
   })
 }
@@ -19,7 +16,7 @@ const deleteUser = async (req, res) => {
   try {
     const user = await models.Users.findOne({ where: { id: uid } });
     if (!user) {
-      res.status(404).json(`usuaria solicitada não encontrada`)
+      res.json(error(404, 'usuario(a) não existe'))
     } else {
       const deleteUser = await models.Users.destroy({   // dá pra aproveitar o user??
         where: {
@@ -27,32 +24,25 @@ const deleteUser = async (req, res) => {
         }
       })
       if (deleteUser === 1) {
-        res.status(200).json(rebuildObj([user]))
+        res.status(200).json(rebuildObj([user])[0])
       }
     }
   } catch (error) {
     console.log(error)
-    res.json("error")
   }
 }
 
 const getAllUsers = async (req, res) => {
   // requer auth
-  // headers  
   try {
     const users = await models.Users.findAll({
       order: [
         ['id', 'ASC']
       ]
     })
-    if (users.length > 0) {
-      res.json(rebuildObj(users))
-    } else {
-      res.json('não tem usuarios')
-    }
+    res.json(rebuildObj(users))
   } catch (error) {
     console.log(error)
-    res.json("caiu no catch do get all users", error)
   }
 }
 
@@ -60,53 +50,58 @@ const getUser = async (req, res) => {
   // requer auth
   const { uid } = req.params
   try {
-    const user = await models.Users.findAll({
+    const user = await models.Users.findOne({
       where: {
         id: uid
       }
     })
-    if (user.length > 0) {
-      res.json(rebuildObj(user))
+    if (user) {
+      res.json(rebuildObj([user])[0])
     } else {
-      res.json('usuaria solicitada no existe')
+      res.status(404).json('usuaria solicitada no existe')
     }
   } catch (error) {
-    res.json('caiu no catch do getUser by id')
+    console.log(error)
   }
 }
 
 const postUser = async (req, res) => {
-  const { email, password, name, role, admin } = req.body
+  const { email, password, name, role } = req.body
 
-  if (email && password && (role.toLowerCase() === "hall" || role.toLowerCase() === "cook")) {
+  if (!email || !password || !role) {
+    res.json(error(400, "Email, senha ou função não fornecido"))
+  } else {
     try {
-      // const saltRounds = 12;
-      // bcrypt.hash({ email }, saltRounds, function (err, hash) {
-      // console.log(hash)
-      // Store hash in your password DB.
-      const user = await models.Users.create({
-        name,
-        email,
-        // password: hash,
-        admin: admin || false,
-        role: role.toLowerCase()
-      });
-      // });      
+      const hasUser = await models.Users.findOne({ where: { email } })
+      if (!hasUser) {
+        const saltRounds = 12;
+        const hash = await bcrypt.hash(password, saltRounds);
+        const user = await models.Users.create({
+          name,
+          email,
+          password: hash,
+          role: role.toLowerCase(),
+        });
+        const token = jwt.sign({ email, id: user.id, role: user.role }, 'HMAC', { expiresIn: "1y" })
+        user.dataValues.token = token
+        res.json(rebuildObj([user])[0])
+      } else {
+        res.json(error(403, "Email já cadastrado"))
+      }
     } catch (error) {
       console.log(error)
     }
-  } else {
-    console.log("falta campo a ser preenchido")
   }
-  res.json({ email, password })
 }
 
 const updateUser = async (req, res) => {
-  const { email, password, name, role, admin } = req.body
+  const { email, password, name, role } = req.body
   const { uid } = req.params
-  if (email && password && uid) {
+  if (!email || !password) {
+    res.json(error(400, "Email ou senha não fornecido"))
+  } else {
     try {
-      const user = await models.Users.findOne({ where: { id: uid } });
+      const user = await models.Users.findOne({ where: { id: uid, email } });
       let updatedUser = user.dataValues
       if (name) {
         updatedUser.name = name
@@ -114,20 +109,15 @@ const updateUser = async (req, res) => {
       if (role) {
         updatedUser.role = role
       }
-      if (admin) {
-        updatedUser.admin = admin
-      }
       await models.Users.update(updatedUser, {
         where: {
           id: uid
         }
       });
-      res.status(200).json(updatedUser)
+      res.status(200).json(rebuildObj([user])[0])
     } catch (error) {
       console.log(error);
     }
-  } else {
-    res.json("não tem senha, email ou id")
   }
 }
 
