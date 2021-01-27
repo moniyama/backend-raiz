2652581f747ea3915ee49953f993185061bebdb3
Monica <monicasayama@gmail.com>
2021-01-27 16:33:01 -0300

    fix bug lowercase

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const ordersRouter = require('./server/routes/OrdersRouter')
 const usersRouter = require('./server/routes/UsersRouter')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000 
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
