57d558d9b55d88da88257984a93a214e6a212a80
Monica <monicasayama@gmail.com>
2021-09-17 18:54:01 -0300

    auth errors

--- server/controller/AuthController.js
+++ server/controller/AuthController.js
@@ -6,22 +6,26 @@ const error = require('../../utils')
 const createToken = async (req, res) => {
   const { email, password } = req.body
   if (!email || !password) {
-    res.status(400).json(error(400, 'email/senha não fornecido'))
+    throw new Error(400, 'email/senha não fornecido')
   } else {
     try {
       const user = await models.Users.findOne({ where: { email } })
-      const userData = user.dataValues
-      const passwordIsRight = await bcrypt.compare(password, userData.password)
-      if (user && passwordIsRight) {
-        const token = jwt.sign({ email, id: userData.id }, 'HMAC', { expiresIn: "1y" })
-        userData.token = token
-        delete userData.password
-        res.status(200).json(userData)
+      if (user) {
+        const userData = user.dataValues
+        const passwordIsRight = await bcrypt.compare(password, userData.password)
+        if (passwordIsRight) {
+          const token = jwt.sign({ email, id: userData.id }, 'HMAC', { expiresIn: "1y" })
+          userData.token = token
+          delete userData.password
+          return res.status(200).json(userData)
+        } else {
+          throw new Error("email/senha inválido")
+        }
       } else {
-        throw new Error()
+        throw new Error("email/senha inválido")
       }
     } catch (err) {
-      res.status(400).json(error(400, "email/senha inválido"))
+      return res.status(400).json(error(400, err.message));
     }
   }
 }
