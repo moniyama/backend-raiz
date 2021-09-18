601e7d1a019dd1225e9bce5466c4a51f2a8fa831
Monica <monicasayama@gmail.com>
2021-09-17 23:57:46 -0300

    refact middleware auth errors

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,29 +1,31 @@
 const jwt = require('jsonwebtoken')
 const models = require('../db/models')
-const error = require('../../utils')
 
 const tokenVerify = async (req, res, next) => {
   const token = req.headers.authorization
-  if (!token) {
-    res.status(401).json(error(401, "token não fornecido"))
-  } else {
-    try {
-      const decoded = jwt.verify(token, 'HMAC')
-      const hasUser = await models.Users.findOne({
-        where: {
-          id: decoded.id,
-          email: decoded.email
-        }
-      })
-      res.locals.user = hasUser;
-      if (hasUser) {
-        next()
-      } else {
-        throw new Error()
-      }
-    } catch (err) {
-      res.status(401).json(error(401, "token inválido"))
+  const errors = {
+    invalidToken: { code: 401, message: "token inválido" },
+    missingToken: { code: 401, message: "token não fornecido" }
+  }
+  try {
+    if (!token) {
+      throw (errors.missingToken)
     }
+    const decoded = jwt.verify(token, 'HMAC')
+    const { id, email } = decoded
+    const hasUser = await models.Users.findOne({
+      where: {
+        id,
+        email
+      }
+    })
+    res.locals.user = hasUser;
+    if (!hasUser) {
+      throw (errors.invalidToken)
+    }
+    next()
+  } catch (err) {
+    return res.status(err.code).json(err)
   }
 }
 
